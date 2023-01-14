c023344512bc5a8ea913d2bb27d325316c9b1792
Kevin Wu <kevwuhoo15@gmail.com>
2023-01-14 18:32:22 -0500

    get login token

--- node-backend/routes/users.js
+++ node-backend/routes/users.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
+  let token = req.body.credential;
+  let CLIENT_ID = req.body.clientId;
   // get req.body.credentials
+  const {OAuth2Client} = require('google-auth-library');
+const client = new OAuth2Client(CLIENT_ID);
+async function verify() {
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      // Or, if multiple clients access the backend:
+      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  console.log("TEST")
+  const payload = ticket.getPayload();
+  const userid = payload['sub'];
+  // If request specified a G Suite domain:
+  // const domain = payload['hd'];
+  console.log(payload)
+}
+verify().catch(console.error);
+
   // parse cred
   // verify cred
   // 
