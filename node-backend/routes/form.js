3f837c605d8ba017c4f2a8adf06410431df9b611
rytuph <ryanzhang50@gmail.com>
2023-01-15 00:43:05 -0500

    error checks for data submission

--- node-backend/routes/form.js
+++ node-backend/routes/form.js
@@ -9,6 +9,10 @@ router.post('/', function(req, res, next) {
   if (!type_name){
     type_name = 'NULL'
   }
+  let food_name = req.body.food_name;
+  if (!food_name){
+    food_name = 'NULL'
+  }
   let lat = req.body.lat;
   if (!lat){
     res.sendStatus(400);
@@ -16,6 +20,14 @@ router.post('/', function(req, res, next) {
   let long = req.body.long;
   if (!long){
     res.sendStatus(400);
+  }
+  let truck_desc = req.body.truck_desc;
+  if (!truck_desc){
+    truck_desc = 'NULL'
+  }
+  let rating = req.body.rating;
+  if (!rating){
+    rating = 'NULL'
   }
   pool.query(
     'INSERT INTO appdata (type_name, food_name, lat, lng, truck_desc, rating) VALUES ($1, $2, $3, $4, $5, $6);',
