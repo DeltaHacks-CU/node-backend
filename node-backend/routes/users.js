e380bb95262f153259bc07e6361593f2fffb949f
Kevin Wu <kevwuhoo15@gmail.com>
2023-01-14 19:24:32 -0500

    cleaned up file

--- node-backend/routes/users.js
+++ node-backend/routes/users.js
@@ -1,33 +1,25 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.post('/', function(req, res, next) {
+router.post("/", function (req, res, next) {
   let token = req.body.credential;
   let CLIENT_ID = req.body.clientId;
-  // get req.body.credentials
-  const {OAuth2Client} = require('google-auth-library');
-const client = new OAuth2Client(CLIENT_ID);
-async function verify() {
-  const ticket = await client.verifyIdToken({
+  const { OAuth2Client } = require("google-auth-library");
+  const client = new OAuth2Client(CLIENT_ID);
+  async function verify() {
+    const ticket = await client.verifyIdToken({
       idToken: token,
-      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-      // Or, if multiple clients access the backend:
-      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-  });
-  console.log("TEST")
-  const payload = ticket.getPayload();
-  const userid = payload['sub'];
-  // If request specified a G Suite domain:
-  // const domain = payload['hd'];
-  console.log(payload)
-}
-verify().catch(console.error);
+      audience: CLIENT_ID,
+    });
+    const payload = ticket.getPayload();
+    const userid = payload["sub"];
 
-  // parse cred
-  // verify cred
-  // 
-  res.send('respond with a resource');
+    console.log(payload);
+  }
+  verify().catch(console.error);
+
+  res.send("respond with a resource");
 });
 
 module.exports = router;
