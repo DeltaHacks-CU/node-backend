94f4285b952dff3eba36ba29d54d590880107c26
colin_mccormack <colin.william.mccormack@gmail.com>
2023-01-14 19:38:10 -0500

    preliminary logged in struct

---
+++ node-backend/routes/loggedin.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    // if logged in serve res.reder logged in
+    // else redirect to index.html
+  res.render('loggedin', { title: 'Home' });
+});
+
+module.exports = router;
